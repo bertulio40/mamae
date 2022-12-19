1bbd2cc6a7ff49203dee0bb82928a74bc8ebcb0f
bertulio40 <109047366+bertulio40@users.noreply.github.com>
2022-12-18 22:22:48 -0300

    Initial commit

---
+++ src/CadastroMamae.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CadastroMamae {
+
+    public static void main(String[] args) {
+
+        String nome;
+        int cpf,plano;
+
+        String digiteONomeDoCliente = JOptionPane.showInputDialog("Digite o nome do cliente");
+        cpf = Integer.parseInt(String.valueOf(Integer.parseInt(JOptionPane.showInputDialog("Digite o CPF Do Cliente"))));
+         plano = (Integer.parseInt(JOptionPane.showInputDialog("Digite o Valor Do Plano")));
+
+
+
+    }
+
+}
+
+
+
+
+
